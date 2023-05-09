3c2484a9575897287f7e56629de9fe50833d8ae3
dangolbeeker <scottb305@outlook.com>
2023-05-09 14:05:48 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./index.js", // main js
+  output: {
+    path: path.resolve(__dirname, "dist"), // output folder
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader", // for styles
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html", // base html
+    }),
+  ],
+};
